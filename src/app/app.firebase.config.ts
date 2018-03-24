4a3cd0e302040f8f7d184ef62824ba0b109764a8
simonvherzele <simon.vanherzele@gmail.com>
2018-03-24 17:58:22 +0100

    project

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyACGBwXFgurz7Ie3ZF5_8vJU2Fwsm3CLhY",
+    authDomain: "roamerslife-d89eb.firebaseapp.com",
+    databaseURL: "https://roamerslife-d89eb.firebaseio.com",
+    projectId: "roamerslife-d89eb",
+    storageBucket: "roamerslife-d89eb.appspot.com",
+    messagingSenderId: "855350109844"
+  };
---
+++ src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+
+  constructor( private afAuth: AngularFireAuth, private toast: ToastController,
+  public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+  	this.afAuth.authState.subscribe(data => {
+  	if(data.email && data.uid){
+  		this.toast.create({
+  			message: `Welcome to The Roamers Life, ${data.email}`,
+  			duration: 3000
+  		}).present();
+  	}
+  	else{
+  		this.navCtrl.setRoot('LoginPage');
+  	}
+  	})
+  }
+
+}
